b2ac44484e636dc99841b10f27503e0b357b299d
Alexander Koval <kovalidis@gmail.com>
2016-12-27 20:12:41 +0200

    Use vagga to run tests on travis

--- jmorphy2-elasticsearch/src/main/java/company/evo/jmorphy2/elasticsearch/indices/Jmorphy2Service.java
+++ jmorphy2-elasticsearch/src/main/java/company/evo/jmorphy2/elasticsearch/indices/Jmorphy2Service.java
@@ -1,8 +1,25 @@
+/*
+ * Copyright 2016 Alexander Koval
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package company.evo.jmorphy2.elasticsearch.indices;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Path;
 import java.nio.file.Files;
@@ -42,7 +59,8 @@ public class Jmorphy2Service extends AbstractComponent {
 
     public Jmorphy2Service(final Settings settings) throws IOException {
         super(settings);
-        this.jmorphy2Dir = resolveJmorphy2Directory(settings);
+        Environment env = new Environment(settings);
+        this.jmorphy2Dir = resolveJmorphy2Directory(settings, env);
         this.defaultCacheSize = settings.getAsInt
             ("cache_size", DEFAULT_CACHE_SIZE);
         this.taggerThreshold = settings.getAsInt
@@ -90,20 +108,21 @@ public class Jmorphy2Service extends AbstractComponent {
         Path extractorRulesPath = path.resolve("extract_rules.txt");
         String extractorRules;
         if (Files.isRegularFile(extractorRulesPath)) {
-            extractorRules = new String(Files.readAllBytes(extractorRulesPath));
+            extractorRules = new String(Files.readAllBytes(extractorRulesPath),
+                                        StandardCharsets.UTF_8);
         } else {
             extractorRules = "+NP,nomn +NP,accs -PP -Geox NOUN,nomn NOUN,accs LATN NUMB";
         }
         return new SubjectExtractor(parser, extractorRules, true);
     }
 
-    private Path resolveJmorphy2Directory(Settings settings) {
+    private Path resolveJmorphy2Directory(Settings settings, Environment env) {
+        Path configDir = env.configFile();
         String dictsLocation = settings.get(JMORPHY2_DICT_LOCATION_SETTING, null);
         if (dictsLocation != null) {
-            return PathUtils.get(dictsLocation);
+            return configDir.resolve(dictsLocation);
         }
-        return PathUtils.get(settings.get(Environment.PATH_CONF_SETTING.getKey()))
-            .resolve(DEFAULT_JMORPHY2_DICT_LOCATION);
+        return configDir.resolve(DEFAULT_JMORPHY2_DICT_LOCATION);
     }
 
     private MorphAnalyzer loadMorphAnalyzer(Path path)
