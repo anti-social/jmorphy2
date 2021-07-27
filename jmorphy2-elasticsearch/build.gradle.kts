c02f7cbfa811ced8d33afef61d58f442f943e4b8
Alexander Koval <kovalidis@gmail.com>
2021-07-27 18:30:54 +0300

    Debian package

--- jmorphy2-elasticsearch/build.gradle.kts
+++ jmorphy2-elasticsearch/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.nio.file.Paths
+
 buildscript {
     repositories {
         mavenCentral()
@@ -11,8 +13,14 @@ buildscript {
 apply(plugin="idea")
 apply(plugin="elasticsearch.esplugin")
 
+plugins {
+    id("nebula.ospackage") version Versions.nebula
+}
+
+val pluginName = "analysis-jmorphy2"
+
 configure<org.elasticsearch.gradle.plugin.PluginPropertiesExtension> {
-    name = "analysis-jmorphy2"
+    name = pluginName
     description = "Jmorphy2 plugin for ElasticSearch"
     classname = "company.evo.jmorphy2.elasticsearch.plugin.AnalysisJmorphy2Plugin"
     version = project.version.toString()
@@ -20,10 +28,12 @@ configure<org.elasticsearch.gradle.plugin.PluginPropertiesExtension> {
     noticeFile = project.file("NOTICE.txt")
 }
 
-
 val libVersion: String = project.getLibraryVersion()
 
 version = "${libVersion}-es${project.getElasticsearchVersion()}"
+val esVersions = org.elasticsearch.gradle.VersionProperties.getVersions() as Map<String, String>
+
+val distDir = Paths.get(buildDir.path, "distributions")
 
 dependencies {
     implementation(project(":jmorphy2-lucene"))
@@ -35,3 +45,28 @@ dependencies {
 tasks.findByName("validateNebulaPom")?.enabled = false
 // 7.13 and after
 tasks.findByName("validateElasticPom")?.enabled = false
+
+
+tasks.named("assemble") {
+    dependsOn("deb")
+}
+
+tasks.register("deb", com.netflix.gradle.plugins.deb.Deb::class) {
+    dependsOn("bundlePlugin")
+
+    packageName = "elasticsearch-${pluginName}-plugin"
+    requires("elasticsearch", esVersions["elasticsearch"])
+        .or("elasticsearch-oss", esVersions["elasticsearch"])
+
+    from(zipTree(tasks["bundlePlugin"].outputs.files.singleFile))
+
+    val esHome = project.properties["esHome"] ?: "/usr/share/elasticsearch"
+    into("$esHome/plugins/${pluginName}")
+
+    doLast {
+        if (properties.containsKey("assembledInfo")) {
+            distDir.resolve("assembled-deb.filename").toFile()
+                .writeText(assembleArchiveName())
+        }
+    }
+}
